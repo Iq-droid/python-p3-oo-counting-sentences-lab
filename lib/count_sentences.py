6cf672f03751d82cd7e105c23e6243941b917556
Iqra Ali <iqra.ali@student.moringaschool.com>
2023-10-11 01:03:23 +0300

    redone

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -4,31 +4,42 @@ import re
 class MyString:
     def __init__(self, value=""):
         if not isinstance(value, str):
-            raise ValueError("The value must be a string.")
-        self.value = value
+          self.value = ""
+          print("The value must be a string.")
+        else:
+            self.value = value
         
     def __str__(self):
         return self.value
     
+    def is_sentence(self):
+      return self.value.endswith('.')
+    
+    def is_question(self):
+      return self.value.endswith('?')
+    
+    def is_exclamation(self):
+      return self.value.endswith('!')
+    
     def ends_with_period(self):
         if not self.value:
-            return False
+          return False
         return self.value.endswith('.')
     
     def ends_with_question_mark(self):
         if not self.value:
-            return False
+          return False
         return self.value.endswith('?')
   
     def ends_with_exclamation_mark(self):
         if not self.value:
-            return False
+          return False
         return self.value.endswith('!')
     
     def count_sentences(self):
         if not self.value:
             return 0
-      #splitting sentences based on the endmark
+      #splitting sentences based on the end mark
         sentences = re.split(r'[.?!]', self.value)
         sentences = [s.strip() for s in sentences if s.strip()]
         return len(sentences)
